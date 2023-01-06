4dcf5f8d073216f77a3e13ecaaba0b2801c586d4
Jack Allen <jackallen@bulb.co.uk>
2023-01-06 10:54:33 +0000

    select key and edcode

--- env-copy.sh
+++ env-copy.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 flags() {
   while test $# -gt 0; do
@@ -21,6 +21,17 @@ flags() {
       export KEY="$1"
       shift
       ;;
+    -d | --decode)
+      shift
+      export DECODE=1
+      shift
+      ;;
+    --help)
+      shift
+      echo "env-copy -p <port> [host] -k <KEY_NAME>"
+      exit 0
+      shift
+      ;;
     *)
       export HOST="$1"
       shift
@@ -31,13 +42,49 @@ flags() {
 
 flags "$@"
 
-if [ -z "$KEY" ]
-then
+if [ -z "$KEY" ]; then
   KEY="SERVICE_KEY"
 fi
 
+if [ -z "$PORT" ]; then
+  echo "I need a port"
+  exit 1
+fi
+
+if [ -z "$HOST" ]; then
+  echo "I need a host"
+  exit 1
+fi
+
+run() {
+  echo $(ssh -p "$PORT" "$HOST" -oStrictHostKeyChecking=accept-new "bash -c '$1'")
+}
+
 echo "Retrieving $KEY from $HOST:$PORT..."
 
-output=$(ssh -p "$PORT" "$HOST" -oStrictHostKeyChecking=accept-new  "bash -c 'env | grep $KEY'")
+run "echo Connected"
 
-echo $output
+keys=$(run "env | grep $KEY | cut -d '=' -f1")
+
+keys=($keys)
+
+i=0
+
+for key in "${keys[@]}"; do
+  echo "$i: $key"
+  i=$((i + 1))
+done
+
+read -p "Which key would you like? [0-9]: " -r key_index
+
+echo "Printing... ${keys[$key_index]}"
+
+value=$(run "echo \$${keys[$key_index]}")
+
+echo "Raw:"
+echo "$value"
+
+if [[ $DECODE == 1 ]]; then
+  echo "Decoded:"
+  echo "$value" | base64 -d
+fi
